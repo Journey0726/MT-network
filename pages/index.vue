fc73ba8f4e9b80a9fe801bc49f144a47893486b3
chenhui <1549120948@qq.com>
2021-03-08 18:50:41 +0800

    index

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
    <el-row>
      <el-col :span="5"> <my-menu/></el-col>
      <el-col :span="19">
-      1
+      <life/>
      </el-col>
      <el-col :span="24">1</el-col>
    </el-row>
@@ -13,9 +13,10 @@
 
 <script>
 import myMenu from '@/components/index/menu.vue'
+import life from '@/components/index/life.vue'
 export default {
   components:{
-    myMenu
+    myMenu,life
   }
 }
 </script>
